1f4fea634d1bfe80575c030e1d0dcdd4deddd0db
Jadon Yariv <jadonyariv@Jadons-MacBook-Air-2.local>
2024-11-16 13:21:39 -0500

    adding file

---
+++ L4/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld { // Define a public class named HelloWorld
+    public static void main(String[] args) { // Main method where the program execution begins
+        // This is where we print stuff out
+        System.out.println("Hello World"); // Print "Hello World" to the console
+        // The above line uses the System.out.println method to print the string "Hello World" to the console.
+        
+        System.out.println("Hola Mundo"); // Print "Hola Mundo" (Spanish for "Hello World") to the console
+        // This line prints a greeting in Spanish to the console.
+        
+        System.out.println("Bonjour le Monde"); // Print "Bonjour le Monde" (French for "Hello World") to the console
+        // This line prints a greeting in French to the console.
+        
+        System.out.println("Ciao mondo"); // Print "Ciao mondo" (Italian for "Hello World") to the console
+        // This line prints a greeting in Italian to the console.
+    }
+}
